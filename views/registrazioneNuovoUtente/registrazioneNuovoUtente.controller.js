ce4cf5a0b6e33a91980fc053be2091b86c3bb22d
Marco Sannicandro <marco.sannicandro87@gmail.com>
2016-05-27 01:26:59 +0200

    Funzione di inserimento/cancellazione utenti Funzione di modifica/elimina eventi Funzione per associare/rimuovere utenti ad eventi Gestione rimozione utenti registrati ad eventi Aggiunta immagini Correzione pulizia form di creazione evento per correggere una eccezione

--- views/registrazioneNuovoUtente/registrazioneNuovoUtente.controller.js
+++ views/registrazioneNuovoUtente/registrazioneNuovoUtente.controller.js
@@ -7,35 +7,67 @@ Per ogni utente si vuole salvare:
 - email aziendale (email)
 - se è esterno oppure no (bool)
 
-*/
+ */
 
 
 (function() {
-  'use strict';
+	'use strict';
 
-  angular
-    .module('engLabEvents')
-    .controller('registrazioneNuovoUtente', registrazioneNuovoUtente);
+	angular
+	.module('engLabEvents')
+	.controller('registrazioneNuovoUtente', registrazioneNuovoUtente);
 
-  registrazioneNuovoUtente.$inject = ['$rootScope'];
+	registrazioneNuovoUtente.$inject = ['$rootScope'];
 
-  /* @ngInject */
-  function registrazioneNuovoUtente($rootScope) {
-    var ctrl = this;
+	/* @ngInject */
+	function registrazioneNuovoUtente($rootScope) {
+		var ctrl = this;
+		
+		ctrl.utente = {
+				nome:null,
+				cognome:null,
+				email:null
+
+		};
+		ctrl.utenti = $rootScope.utenti;
 
 
-    /*
+		/*
       ATTRIBUTI
-     */
+		 */
+		
 
-    /*
+		/*
     Variabili di flusso
-     */
+		 */
 
 
-    /*
+		/*
       FUNZIONI
-     */
+		 */
+		ctrl.salva = salva;
+		ctrl.pulisci = pulisci;
+		ctrl.elimina = elimina;
 
-  }
+		function salva(){
+			if (!$rootScope.utenti) {
+				$rootScope.utenti = [];
+			}
+			ctrl.utente.id = Math.ceil(Math.random() * 100);
+			$rootScope.utenti.push(ctrl.utente);
+			ctrl.utenti=$rootScope.utenti;
+			ctrl.pulisci();
+		}
+
+		function pulisci() {
+			ctrl.utente = {};
+		}
+		
+		function elimina(utente){
+			$rootScope.utenti.splice(utente, 1);
+			ctrl.utenti=$rootScope.utenti;
+	    }
+
+
+	}
 })();
