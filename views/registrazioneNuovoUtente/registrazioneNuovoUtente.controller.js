f98d965878098e3b2d0f4ee4d30f244b6f46bf2d
Alessandro Avolio <avolio+github@gmail.com>
2016-06-13 14:27:46 +0200

    pulito la gestione admin e sicurezza sulla pagina di registrazione

--- views/registrazioneNuovoUtente/registrazioneNuovoUtente.controller.js
+++ views/registrazioneNuovoUtente/registrazioneNuovoUtente.controller.js
@@ -11,84 +11,85 @@ Per ogni utente si vuole salvare:
 
 
 (function() {
-	'use strict';
+  'use strict';
 
-	angular
-	.module('engLabEvents')
-	.controller('registrazioneNuovoUtente', registrazioneNuovoUtente);
+  angular
+    .module('engLabEvents')
+    .controller('registrazioneNuovoUtente', registrazioneNuovoUtente);
 
-	registrazioneNuovoUtente.$inject = ['$localStorage'];
+  registrazioneNuovoUtente.$inject = ['$localStorage'];
 
-	/* @ngInject */
-	function registrazioneNuovoUtente($localStorage) {
-		var ctrl = this;
-		/*
-			ATTRIBUTI
-		 */
-		ctrl.utente = {
-				nome:null,
-				cognome:null,
-				email:null,
-				password:null
+  /* @ngInject */
+  function registrazioneNuovoUtente($localStorage) {
+    var ctrl = this;
+    /*
+    	ATTRIBUTI
+     */
+    ctrl.utente = {
+      nome: null,
+      cognome: null,
+      email: null,
+      password: null
 
-		};
-		ctrl.utenti = $localStorage.utenti;
-
-
+    };
+    ctrl.utenti = $localStorage.utenti;
 
 
-		/*
+
+    /*
     Variabili di flusso
 		 */
+    ctrl.visualizaElimina = false;
+    ctrl.visualizzaUtenti = false;
 
-
-		/*
+    /*
       FUNZIONI
 		 */
-		ctrl.salva = salva;
-		ctrl.pulisci = pulisci;
-		ctrl.elimina = elimina;
+    ctrl.salva = salva;
+    ctrl.pulisci = pulisci;
+    ctrl.elimina = elimina;
 
-		function salva(){
-			for(var i=0; i<ctrl.utenti; i++){
-					if(ctrl.utente.email === ctrl.utenti[i].email){
-						alert('Già presente un utente con la stessa mail. Contattare gli amministratori.');
-					}
-			}
+    function salva() {
+      for (var i = 0; i < ctrl.utenti; i++) {
+        if (ctrl.utente.email === ctrl.utenti[i].email) {
+          alert('Già presente un utente con la stessa mail. Contattare gli amministratori.');
+        }
+      }
 
-			ctrl.utente.id = Math.ceil(Math.random() * 100);
-			ctrl.utente.dataIscrizione = new Date();
-
-			//utente non amministratore
-			ctrl.utente.tipologia = 2;
-			//privilege escalation
-			$localStorage.amministratori.forEach(function(amministratore){
-				if(ctrl.utente.email === amministratore){
-					//utente amministratore
-					ctrl.utente.tipologia = 1;
-				}
-			});
-
-			ctrl.utenti.push(ctrl.utente);
-			ctrl.pulisci();
-		}
-
-		function pulisci() {
-			ctrl.utente = {};
-		}
-
-		function elimina(utente){
-			ctrl.utenti.splice(utente, 1);
-		}
+      ctrl.utente.id = Math.ceil(Math.random() * 100);
+      ctrl.utente.dataIscrizione = new Date();
 
 
-		function init(){
-			if (!$localStorage.utenti) {
-				$localStorage.utenti = [];
-			}
-				ctrl.utenti = $localStorage.utenti;
-		}
-		init();
+      ctrl.utenti.push(ctrl.utente);
+      ctrl.pulisci();
+    }
 
-	}
+    function pulisci() {
+      ctrl.utente = {};
+    }
+
+    function elimina(utente) {
+      if ($localStorage.utenteLoggato.ruolo != 'admin') {
+        alert('Non sei autorizzato a effettuare questa operazione');
+        return;
+      }
+      ctrl.utenti.splice(utente, 1);
+    }
+
+
+    function init() {
+      if (!$localStorage.utenti) {
+        $localStorage.utenti = [];
+      }
+      ctrl.utenti = $localStorage.utenti;
+      if ($localStorage.utenteLoggato) {
+        ctrl.visualizzaUtenti = true;
+        if ($localStorage.utenteLoggato && $localStorage.utenteLoggato.ruolo === 'admin') {
+          ctrl.visualizaElimina = true;
+        }
+      }
+    }
+    init();
+
+  }
 })();
